c3adf280847068bd0e2148d50bf78ccc213164bc
Angel Gonzalez <angelvicente0306@hotmail.com>
2025-02-05 11:57:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let moves, totalMoves, current;
+const sonido = new Audio("8-bit-victory-sound-101319.mp3");
+
+function iluminate(cellPos, time)   {
+    setTimeout(() => {
+        document.querySelector('.cell[pos="' + cellPos + '"]').classList.add('active');
+        sonido.play();
+        setTimeout(() => {
+            document.querySelector('.cell[pos="' + cellPos + '"]').classList.remove('active'); 
+        }, 300);
+    }, time);
+}
+
+function setMoves() {
+    moves.push(Math.floor(Math.random() * 4) + 1);
+    if (current < totalMoves) {
+        current++;
+        setMoves();
+    }
+}
+
+function startGame() {
+    moves = [];
+    totalMoves = 2;
+    current = 0;
+    document.querySelector('#start').style.display = 'none';
+    document.querySelector('#message').style.display = 'block';
+    sequence();
+}
+
+function sequence() {
+    moves = [];
+    current = 0;
+    setMoves();
+    document.querySelector('#message').innerHTML = "Simon dice";
+
+    for (let i = 0; i < moves.length; i++) {
+        iluminate(moves[i], 600 * i);   
+    }
+
+    setTimeout(() => {
+        document.querySelector('#message').innerHTML = 'Replicate the sequence';
+    }, 600 * moves.length);
+}
+
+function cellClick(e) {
+    let cellPos = e.target.getAttribute('pos');
+    iluminate(cellPos, 0);
+
+    if (moves[0] && moves.length) {
+        if (moves[0] == cellPos) {
+            moves.shift();
+
+            if (!moves.length) {
+                totalMoves++;
+                setTimeout(() => {
+                    sequence();
+                }, 1000);
+            }
+        } else {
+            document.querySelector('#message').innerHTML = 'Perdiste';
+            setTimeout(() => {
+                document.querySelector('#start').style.display = 'block';
+                document.querySelector('#message').style.display = 'none';
+            }, 1000);
+        }
+    }
+}
+
+document.querySelector('#start').addEventListener('click', startGame);
+let cells = Array.from(document.getElementsByClassName('cell'));
+
+cells.forEach(cell => {
+    cell.addEventListener('click', cellClick);
+});
